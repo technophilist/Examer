0d4e79a6602706632e09c5f8242ac466d7808ff5
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-01-23 14:22:12 +0530

    refactor!: rename 'NavigationDrawerItem' data class
    This commit renames 'NavigationDrawerItem' data class
    to NavigationDrawerDestination

--- app/src/main/java/com/example/examer/ui/screens/ExamerNavigation.kt
+++ app/src/main/java/com/example/examer/ui/screens/ExamerNavigation.kt
@@ -3,28 +3,22 @@ package com.example.examer.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.examer.R
 import com.google.accompanist.insets.statusBarsHeight
 import com.google.accompanist.insets.statusBarsPadding
 
 
-data class NavigationDrawerItem(
+data class NavigationDrawerDestination(
     val icon: ImageVector,
     val name: String,
     val onClick: () -> Unit,
@@ -37,7 +31,7 @@ fun ExamerNavigation(
     onNavigationIconClick: (() -> Unit)? = null,
     onNavigationItemClick: ((index: Int) -> Unit)? = null,
     currentlySelectedNavigationDrawerItemIndex: Int = 0,
-    navigationDrawerDestinations: List<NavigationDrawerItem>,
+    navigationDrawerDestinations: List<NavigationDrawerDestination>,
     content: @Composable (PaddingValues) -> Unit,
 ) {
     // TODO State hoisting
@@ -93,7 +87,7 @@ private fun ExamerAppbar(
 private fun DrawerItem(
     isSelected: Boolean = false,
     onItemSelected: (() -> Unit)? = null,
-    navigationDrawerItem: NavigationDrawerItem,
+    navigationDrawerItem: NavigationDrawerDestination,
 ) {
     //TODO Hoist color state
     Row(
