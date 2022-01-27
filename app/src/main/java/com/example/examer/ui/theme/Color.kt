ed334e17aca007f6bffa5d544e74b66154b884c5
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-01-27 10:42:30 +0530

    refactor: add Green200 and Green600 colors

--- app/src/main/java/com/example/examer/ui/theme/Color.kt
+++ app/src/main/java/com/example/examer/ui/theme/Color.kt
@@ -5,16 +5,23 @@ import androidx.compose.ui.graphics.Color
 // Black
 val Black800 = Color(0xFF121212)
 val Black900 = Color(0xFF000000)
+
 // Blue
 val Blue50 = Color(0xFFEEF0F2)
 val Blue200 = Color(0xffadbbc4)
 val Blue300 = Color(0xff8ca2ae)
 val Blue700 = Color(0xFF444955)
 val Blue800 = Color(0xFF232F34)
+
 // Orange
 val Orange300 = Color(0xFFFBD790)
-val Orange400 =  Color(0xFFF9BE64)
+val Orange400 = Color(0xFFF9BE64)
 val Orange500 = Color(0xFFF9AA33)
+
 // Red
 val Red200 = Color(0xFFCF7779)
 val Red400 = Color(0xFFFF4C5D)
+
+// Green
+val Green200 = Color(0xff00d600)
+val Green600 = Color(0xff00a300)
