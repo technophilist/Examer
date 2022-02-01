3a59d969a801adb4ea0c24d40a404e6586219e82
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-02-01 16:33:52 +0530

    refactor: create Repository

---
+++ app/src/main/java/com/example/examer/data/Repository.kt
@@ -0,0 +1,14 @@
+package com.example.examer.data
+
+import com.example.examer.data.domain.ExamerUser
+import com.example.examer.data.domain.TestDetails
+import com.example.examer.data.remote.RemoteDatabase
+
+interface Repository {
+    suspend fun fetchTestListForUser(user: ExamerUser): List<TestDetails>
+}
+
+class ExamerRepository(private val remoteDatabase: RemoteDatabase) : Repository {
+    override suspend fun fetchTestListForUser(user: ExamerUser): List<TestDetails> =
+        remoteDatabase.fetchTestListForUser(user)
+}
