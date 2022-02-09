8d9a5a908893bf6c4815e0e4738695926089d2cd
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-02-09 15:35:34 +0530

    refactor(HomeViewModel): make test list backing property immutable

--- app/src/main/java/com/example/examer/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/examer/viewmodels/HomeViewModel.kt
@@ -11,15 +11,16 @@ import com.example.examer.data.domain.TestDetails
 import kotlinx.coroutines.launch
 
 interface HomeViewModel {
-    var testDetailsList: State<List<TestDetails>>
+    val testDetailsList: State<List<TestDetails>>
 }
 
+// TODO change HomeViewModel to ScheduledTestViewModel
 class ExamerHomeViewModel(
     private val authenticationService: AuthenticationService,
     private val repository: Repository,
 ) : ViewModel(), HomeViewModel {
     private val _testDetailsList: MutableState<List<TestDetails>> = mutableStateOf(listOf())
-    override var testDetailsList: State<List<TestDetails>> = _testDetailsList
+    override val testDetailsList: State<List<TestDetails>> = _testDetailsList
 
     init {
         viewModelScope.launch {
