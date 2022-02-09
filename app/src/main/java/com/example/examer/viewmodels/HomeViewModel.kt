acde5071a88602c00f470620ef432f81cb4049d3
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-02-09 16:11:14 +0530

    refactor(HomeViewModel)!: add UI state val and refresh method

--- app/src/main/java/com/example/examer/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/examer/viewmodels/HomeViewModel.kt
@@ -10,8 +10,14 @@ import com.example.examer.data.Repository
 import com.example.examer.data.domain.TestDetails
 import kotlinx.coroutines.launch
 
+/**
+ * An enum indicating the state of the HomeScreen.
+ */
+enum class HomeScreenUiState { LOADING, SUCCESSFULLY_LOADED }
 interface HomeViewModel {
     val testDetailsList: State<List<TestDetails>>
+    val homeScreenUiState: State<HomeScreenUiState>
+    fun refreshTestDetailsList()
 }
 
 // TODO change HomeViewModel to ScheduledTestViewModel
@@ -20,12 +26,39 @@ class ExamerHomeViewModel(
     private val repository: Repository,
 ) : ViewModel(), HomeViewModel {
     private val _testDetailsList: MutableState<List<TestDetails>> = mutableStateOf(listOf())
+    private var _homeScreenUiState: MutableState<HomeScreenUiState> =
+        mutableStateOf(HomeScreenUiState.LOADING)
     override val testDetailsList: State<List<TestDetails>> = _testDetailsList
+    override val homeScreenUiState: State<HomeScreenUiState> = _homeScreenUiState
 
     init {
+        fetchAndAssignTestDetailsList()
+    }
+
+    override fun refreshTestDetailsList() {
+        fetchAndAssignTestDetailsList()
+    }
+
+    /**
+     * Used to fetch the list of [TestDetails] associated with the
+     * [AuthenticationService.currentUser] and setting it to the
+     * [_testDetailsList] backing property. It also manages
+     * the [homeScreenUiState].
+     */
+    private fun fetchAndAssignTestDetailsList() {
         viewModelScope.launch {
-            _testDetailsList.value = authenticationService
-                .currentUser?.let { repository.fetchTestListForUser(it) } ?: emptyList()
+            _homeScreenUiState.value = HomeScreenUiState.LOADING
+            _testDetailsList.value = fetchTestListForCurrentUser() ?: emptyList()
+            _homeScreenUiState.value = HomeScreenUiState.SUCCESSFULLY_LOADED
         }
     }
+
+    /**
+     * Used to fetch the list of [TestDetails] associated with the
+     *[AuthenticationService.currentUser]. This function will return
+     * null if the current user is null.
+     */
+    private suspend fun fetchTestListForCurrentUser(): List<TestDetails>? = authenticationService
+        .currentUser?.let { repository.fetchTestListForUser(it) }
+
 }
