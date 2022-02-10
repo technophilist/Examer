0fc2a08e321d939186fb53db63e6db92deaea19d
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-02-10 11:05:39 +0530

    refactor: add fetchPreviousTestListForUser method to Repository

--- app/src/main/java/com/example/examer/data/Repository.kt
+++ app/src/main/java/com/example/examer/data/Repository.kt
@@ -6,9 +6,13 @@ import com.example.examer.data.remote.RemoteDatabase
 
 interface Repository {
     suspend fun fetchScheduledTestListForUser(user: ExamerUser): List<TestDetails>
+    suspend fun fetchPreviousTestListForUser(user: ExamerUser): List<TestDetails>
 }
 
 class ExamerRepository(private val remoteDatabase: RemoteDatabase) : Repository {
     override suspend fun fetchScheduledTestListForUser(user: ExamerUser): List<TestDetails> =
         remoteDatabase.fetchScheduledTestListForUser(user)
+
+    override suspend fun fetchPreviousTestListForUser(user: ExamerUser): List<TestDetails> =
+        remoteDatabase.fetchPreviousTestListForUser(user)
 }
