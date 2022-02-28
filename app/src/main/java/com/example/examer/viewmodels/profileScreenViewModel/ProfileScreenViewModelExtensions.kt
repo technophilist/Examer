2eb2110457d934ed36633374c5148575e4476c01
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-02-28 12:16:49 +0530

    refactor(ProfileScreenViewModel): add resetStateTimeOut param to extensions

--- app/src/main/java/com/example/examer/viewmodels/profileScreenViewModel/ProfileScreenViewModelExtensions.kt
+++ app/src/main/java/com/example/examer/viewmodels/profileScreenViewModel/ProfileScreenViewModelExtensions.kt
@@ -1,23 +1,35 @@
 package com.example.examer.viewmodels.profileScreenViewModel
 
 
-fun ProfileScreenViewModel.updateName(newName: String) {
+fun ProfileScreenViewModel.updateName(
+    newName: String,
+    resetStateTimeOut: Long = defaultResetStateTimeOut
+) {
     updateAttributeForCurrentUser(
         ProfileScreenViewModel.UpdateAttribute.NAME,
         newName,
+        resetStateTimeOut
     )
 }
 
-fun ProfileScreenViewModel.updateEmail(newEmail: String) {
+fun ProfileScreenViewModel.updateEmail(
+    newEmail: String,
+    resetStateTimeOut: Long = defaultResetStateTimeOut
+) {
     updateAttributeForCurrentUser(
         ProfileScreenViewModel.UpdateAttribute.EMAIL,
-        newEmail
+        newEmail,
+        resetStateTimeOut
     )
 }
 
-fun ProfileScreenViewModel.updatePassword(newPassword: String) {
+fun ProfileScreenViewModel.updatePassword(
+    newPassword: String,
+    resetStateTimeOut: Long = defaultResetStateTimeOut
+) {
     updateAttributeForCurrentUser(
         ProfileScreenViewModel.UpdateAttribute.PASSWORD,
-        newPassword
+        newPassword,
+        resetStateTimeOut
     )
 }
