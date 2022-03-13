74d314927568a1af04fb583dc16852bbd7b247d8
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-13 11:53:07 +0530

    refactor: updates docs for ExamerUser model class

--- app/src/main/java/com/example/examer/data/domain/ExamerUser.kt
+++ app/src/main/java/com/example/examer/data/domain/ExamerUser.kt
@@ -3,9 +3,9 @@ package com.example.examer.data.domain
 import android.net.Uri
 
 /**
- * A domain model class that represents a user with a [name],
- * [email] and a [photoUrl] that will be used as the profile
- * picture of the user.
+ * A domain model class that represents a user with a [id],[name],
+ * [email],[phoneNumber] and a [photoUrl] that will be used as the
+ * profile picture of the user.
  */
 data class ExamerUser(
     val id: String,
