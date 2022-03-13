2df6698bae2215362a911bd5b14ab059d6b817d2
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-13 12:03:16 +0530

    docs: add docs for MultiChoiceQuestionListDTO class

--- app/src/main/java/com/example/examer/data/dto/MultiChoiceQuestionListDTO.kt
+++ app/src/main/java/com/example/examer/data/dto/MultiChoiceQuestionListDTO.kt
@@ -2,6 +2,9 @@ package com.example.examer.data.dto
 
 import kotlinx.serialization.Serializable
 
+/**
+ * A DTO value class that wraps a list of [MultiChoiceQuestionListDTO].
+ */
 @Serializable
 @JvmInline
 value class MultiChoiceQuestionListDTO(val questions: List<MultiChoiceQuestionDTO>)
