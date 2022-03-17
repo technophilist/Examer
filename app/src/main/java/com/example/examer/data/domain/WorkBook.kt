8cc8f6469c81f002d7d77db731fca8ff097eefdd
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-17 14:23:39 +0530

    refactor: mark all domain object execpt ExamerUser with @Serializable

--- app/src/main/java/com/example/examer/data/domain/WorkBook.kt
+++ app/src/main/java/com/example/examer/data/domain/WorkBook.kt
@@ -1,11 +1,14 @@
 package com.example.examer.data.domain
 
+import kotlinx.serialization.Serializable
+
 /**
  * A class that models a work book with the associated [id],
  * [audioFile] and [questions] list.
  */
+@Serializable
 data class WorkBook(
     val id: String,
     val audioFile: ExamerAudioFile,
     val questions: List<MultiChoiceQuestion>
-)
+) : java.io.Serializable
