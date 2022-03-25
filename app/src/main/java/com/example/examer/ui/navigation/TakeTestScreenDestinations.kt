d69578d3032be62986eb306eccd1f2780661d644
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-25 12:09:13 +0530

    refactor(TakeTestScrenNavigation): add nav controller param & WorkBookScreen

--- app/src/main/java/com/example/examer/ui/navigation/TakeTestScreenDestinations.kt
+++ app/src/main/java/com/example/examer/ui/navigation/TakeTestScreenDestinations.kt
@@ -1,5 +1,21 @@
 package com.example.examer.ui.navigation
 
+import com.example.examer.data.domain.MultiChoiceQuestion
+import com.example.examer.data.domain.WorkBook
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import timber.log.Timber
+
 sealed class TakeTestScreenDestinations(val route: String) {
-    object ListenToAudioScreen : TakeTestScreenDestinations("com.example.examer.ui.navigation")
+    object ListenToAudioScreen :
+        TakeTestScreenDestinations("com.example.examer.ui.navigation.WorkBookScreen")
+
+    object WorkBookScreen :
+        TakeTestScreenDestinations("com.example.examer.ui.navigation.WorkBookScreen/{questionsList}/") {
+        const val QUESTIONS_LIST_ARG = "questionsList"
+        fun buildRoute(multiChoiceQuestionList: List<MultiChoiceQuestion>): String {
+            val jsonString = Json.encodeToString(multiChoiceQuestionList)
+            return "com.example.examer.ui.navigation.WorkBookScreen/$jsonString/"
+        }
+    }
 }
