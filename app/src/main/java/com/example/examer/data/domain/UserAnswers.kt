361083aa83504208084c33d1884ab8dec41767a8
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-03-28 12:08:35 +0530

    refactor(UserAnswers): create/add IndexOfChosenOption value class

--- app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
+++ app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
@@ -1,6 +1,9 @@
 package com.example.examer.data.domain
 
+@JvmInline
+value class IndexOfChosenOption(val index: Int)
+
 data class UserAnswers(
     val associatedWorkBookId: String,
-    val answers: Map<MultiChoiceQuestion, Int>
+    val answers: Map<MultiChoiceQuestion, IndexOfChosenOption>
 )
