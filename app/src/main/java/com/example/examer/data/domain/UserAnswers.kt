c715e6f85c00d4c14b87af39999a9e5ea8c13d74
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-28 12:23:34 +0530

    refactor(UserAnswers): add toUserAnswersDTO extension method

--- app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
+++ app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
@@ -1,9 +1,22 @@
 package com.example.examer.data.domain
 
+import com.example.examer.data.dto.UserAnswersDTO
+
 @JvmInline
 value class IndexOfChosenOption(val index: Int)
 
 data class UserAnswers(
     val associatedWorkBookId: String,
     val answers: Map<MultiChoiceQuestion, IndexOfChosenOption>
+)
+
+fun UserAnswers.toUserAnswersDTO() = UserAnswersDTO(
+    associatedWorkBookId = associatedWorkBookId,
+    answersDetailsMap = answers.keys.map {
+        mapOf(
+            "id" to it.id,
+            "indexOfCorrectOption" to it.indexOfCorrectOption.toString(),
+            "indexOfChosenOption" to answers[it].toString()
+        )
+    }
 )
