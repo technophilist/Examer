119d14aa22d84afcf78615b952cfa0bbbbf25cf6
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-30 17:44:33 +0530

    refactor(WorkBookViewModel): use SaveUserAnswersWorker to save user answers

--- app/src/main/java/com/example/examer/viewmodels/WorkBookViewModel.kt
+++ app/src/main/java/com/example/examer/viewmodels/WorkBookViewModel.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.examer.auth.AuthenticationService
 import com.example.examer.data.Repository
 import com.example.examer.data.domain.UserAnswers
+import com.example.examer.data.workers.SaveUserAnswersWorker
 import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 interface WorkBookViewModel {
     fun saveUserAnswersForTestId(userAnswers: UserAnswers, testDetailsId: String)
@@ -22,12 +24,20 @@ class ExamerWorkBookViewModel(
 ) : AndroidViewModel(application), WorkBookViewModel {
     private val workManager = WorkManager.getInstance(application)
     override fun saveUserAnswersForTestId(userAnswers: UserAnswers, testDetailsId: String) {
-        viewModelScope.launch {
-            repository.saveUserAnswersForUser(
-                user = authenticationService.currentUser.value!!,
-                userAnswers = userAnswers,
-                testDetailId = testDetailsId
-            )
+        val workerConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val inputData = Data.Builder().run {
+            val userDetailsJsonString = Json.encodeToString(userAnswers)
+            putString(SaveUserAnswersWorker.KEY_USER_ANSWERS_JSON_STRING_ARG, userDetailsJsonString)
+            putString(SaveUserAnswersWorker.KEY_TEST_DETAILS_ID_ARG, testDetailsId)
+            build()
         }
+        val workRequest = OneTimeWorkRequest.Builder(SaveUserAnswersWorker::class.java)
+            .setConstraints(workerConstraints)
+            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+            .setInputData(inputData)
+            .build()
+        workManager.enqueue(workRequest)
     }
 }
