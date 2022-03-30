77070340020492675994590dbe062cbdf3d65e85
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-03-30 15:45:16 +0530

    refactor(FirebaseRemoteDatabase): add 'runOnCollection' optional param to fetchCollection()

--- app/src/main/java/com/example/examer/data/remote/FirebaseRemoteDatabase.kt
+++ app/src/main/java/com/example/examer/data/remote/FirebaseRemoteDatabase.kt
@@ -6,9 +6,11 @@ import com.example.examer.data.domain.*
 import com.example.examer.data.dto.AudioFileDTO
 import com.example.examer.data.dto.MultiChoiceQuestionListDTO
 import com.example.examer.data.dto.WorkBookDTO
-import com.example.examer.data.dto.toMultiChoiceQuestion
 import com.example.examer.di.DispatcherProvider
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -27,11 +29,10 @@ class FirebaseRemoteDatabase(private val dispatcherProvider: DispatcherProvider)
 
     override suspend fun fetchScheduledTestListForUser(user: ExamerUser): List<TestDetails> =
         withContext(dispatcherProvider.io) {
-            val scheduledTestsCollection = Firebase.firestore
-                .collection(getCollectionPathForTests(user))
-                .whereEqualTo("testStatus", "scheduled")
-                .get()
-                .await()
+            val scheduledTestsCollection = fetchCollection(
+                collectionPath = getCollectionPathForTests(user),
+                runOnCollectionReference = { whereEqualTo("testStatus", "scheduled") }
+            )
             // if no collection exists for the user, which likely indicates
             // that the user is a newly registered user, an empty list will
             // be returned.
@@ -40,11 +41,10 @@ class FirebaseRemoteDatabase(private val dispatcherProvider: DispatcherProvider)
 
     override suspend fun fetchPreviousTestListForUser(user: ExamerUser): List<TestDetails> =
         withContext(dispatcherProvider.io) {
-            val previousTestsCollection = Firebase.firestore
-                .collection(getCollectionPathForTests(user))
-                .whereIn("testStatus", listOf("completed", "missed"))
-                .get()
-                .await()
+            val previousTestsCollection = fetchCollection(
+                collectionPath = getCollectionPathForTests(user),
+                runOnCollectionReference = { whereIn("testStatus", listOf("completed", "missed")) }
+            )
             // if no collection exists for the user, which likely indicates
             // that the user is a newly registered user, an empty list will
             // be returned.
@@ -127,8 +127,20 @@ class FirebaseRemoteDatabase(private val dispatcherProvider: DispatcherProvider)
             .atZone(ZoneId.systemDefault())
             .toLocalDateTime()
 
-    private suspend fun fetchCollection(collectionPath: String) = Firebase.firestore
+    //TODO add doc
+    private suspend fun fetchCollection(
+        collectionPath: String,
+        runOnCollectionReference: (CollectionReference.() -> Query)? = null
+    ): QuerySnapshot = Firebase
+        .firestore
         .collection(collectionPath)
+        .run {
+            // CollectionReference is a subclass of Query.
+            // if the block is not null, run the block
+            // and return the Query object returned by the block.
+            // if block is null, return the collection reference.
+            runOnCollectionReference?.invoke(this) ?: this
+        }
         .get()
         .await()
 
