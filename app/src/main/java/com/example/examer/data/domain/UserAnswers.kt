1b684a77fa29fb39713b24ee40811d65bebeabaf
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-04-01 11:50:55 +0530

    fix: mark UserAnswers/IndexOfChosenOption classes with @Serializable

--- app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
+++ app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
@@ -1,12 +1,15 @@
 package com.example.examer.data.domain
 
 import com.example.examer.data.dto.UserAnswersDTO
+import kotlinx.serialization.Serializable
 
+@Serializable
 @JvmInline
 value class IndexOfChosenOption(val index: Int) {
     override fun toString(): String = index.toString()
 }
 
+@Serializable
 data class UserAnswers(
     val associatedWorkBookId: String,
     val answers: Map<MultiChoiceQuestion, IndexOfChosenOption>
