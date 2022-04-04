8ebe7521348fc280c7477e19cc8ded5b4cd96271
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-04-04 15:31:20 +0530

    refactor: add optional isLastWorkBook arg to WorkBookScreen#buildRoute

--- app/src/main/java/com/example/examer/ui/navigation/TakeTestScreenDestinations.kt
+++ app/src/main/java/com/example/examer/ui/navigation/TakeTestScreenDestinations.kt
@@ -11,17 +11,19 @@ sealed class TakeTestScreenDestinations(val route: String) {
         TakeTestScreenDestinations("com.example.examer.ui.navigation.WorkBookScreen")
 
     object WorkBookScreen :
-        TakeTestScreenDestinations("com.example.examer.ui.navigation.WorkBookScreen/{testDetailsId}/{workBookId}/{questionsList}/") {
+        TakeTestScreenDestinations("com.example.examer.ui.navigation.WorkBookScreen/{testDetailsId}/{workBookId}/{questionsList}/{isLastWorkBook}/") {
         const val WORKBOOK_ID_ARG = "workBookId"
         const val QUESTIONS_LIST_ARG = "questionsList"
         const val TEST_DETAILS_ID_ARG = "testDetailsId"
+        const val IS_LAST_WORKBOOK_ARG = "isLastWorkBook"
         fun buildRoute(
             testDetailsId: String,
             workBookId: String,
-            multiChoiceQuestionList: List<MultiChoiceQuestion>
+            multiChoiceQuestionList: List<MultiChoiceQuestion>,
+            isLastWorkBook: Boolean = false
         ): String {
             val jsonString = Json.encodeToString(multiChoiceQuestionList)
-            return "com.example.examer.ui.navigation.WorkBookScreen/${testDetailsId}/$workBookId/$jsonString/"
+            return "com.example.examer.ui.navigation.WorkBookScreen/${testDetailsId}/$workBookId/$jsonString/${isLastWorkBook}/"
         }
     }
 }
