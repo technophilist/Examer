095c085b538d1c0b25fd230279f08066ee39b900
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-04-05 15:35:23 +0530

    refactor!: rename UpdateProfilePhotoUriUseCase to UpdateProfilePhotoUriDelegate

--- app/src/main/java/com/example/examer/di/AppContainer.kt
+++ app/src/main/java/com/example/examer/di/AppContainer.kt
@@ -11,7 +11,7 @@ import com.example.examer.data.remote.FirebaseRemoteDatabase
 import com.example.examer.data.remote.RemoteDatabase
 import com.example.examer.usecases.ExamerCredentialsValidationUseCase
 import com.example.examer.usecases.ExamerMarkTestAsCompletedUseCase
-import com.example.examer.delegates.UpdateProfilePhotoUriUseCaseImpl
+import com.example.examer.delegates.ExamerUpdateProfileUriDelegate
 import com.example.examer.utils.*
 import com.example.examer.viewmodels.TestDetailsListType
 
@@ -23,7 +23,7 @@ class AppContainer(application: Application) {
     val repository = ExamerRepository(
         context = application,
         remoteDatabase = remoteDatabase,
-        updateProfilePhotoUriUseCase = UpdateProfilePhotoUriUseCaseImpl(
+        updateProfilePhotoUriUseCase = ExamerUpdateProfileUriDelegate(
             authenticationService,
             passwordManager
         )
