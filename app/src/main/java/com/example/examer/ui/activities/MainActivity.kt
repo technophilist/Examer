d0e6223b66209ed88daf2ceb16a8346e5539fc9c
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-04-08 12:01:31 +0530

    feat: display non-dismissable alert dialog if dateTime/TimeZone is set manually.

--- app/src/main/java/com/example/examer/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/examer/ui/activities/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.example.examer.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import com.example.examer.ui.components.AlertDialog
+
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import coil.annotation.ExperimentalCoilApi
+import com.example.examer.R
 import com.example.examer.di.AppContainer
 import com.example.examer.di.ExamerApplication
 import com.example.examer.ui.screens.ExamerApp
 import com.example.examer.ui.theme.ExamerTheme
+import com.example.examer.utils.isDeviceAutomaticDateTimeEnabled
+import com.example.examer.utils.isDeviceAutomaticTimeZoneEnabled
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 class MainActivity : ComponentActivity() {
     private lateinit var appContainer: AppContainer
+    private var isAutomaticDateTimeEnabled by mutableStateOf(false)
+    private var isAutomaticTimeZoneEnabled by mutableStateOf(false)
 
     @ExperimentalCoilApi
     @ExperimentalMaterialApi
@@ -33,6 +38,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExamerTheme {
                 ProvideWindowInsets {
+                    if (!isAutomaticDateTimeEnabled || !isAutomaticTimeZoneEnabled) {
+                        DateTimeZoneAlertDialog()
+                    }
                     Surface(
                         color = MaterialTheme.colors.background,
                         content = { ExamerApp(appContainer) }
@@ -40,6 +48,26 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    @Composable
+    private fun DateTimeZoneAlertDialog() {
+        AlertDialog(
+            title = stringResource(R.string.label_device_time_set_manually_warning),
+            message = stringResource(R.string.label_device_time_set_manually_message),
+            confirmText = stringResource(R.string.button_label_open_settings).uppercase(),
+            onConfirmButtonClick = {
+                val intent = Intent(android.provider.Settings.ACTION_DATE_SETTINGS)
+                startActivity(intent)
+            },
+            onDismissRequest = { /*Prevent user from closing the dialog*/ }
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        isAutomaticDateTimeEnabled = isDeviceAutomaticDateTimeEnabled()
+        isAutomaticTimeZoneEnabled = isDeviceAutomaticTimeZoneEnabled()
     }
 }
 
