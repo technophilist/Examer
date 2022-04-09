15159e1875ecb3a116a5c48a2394d2656135f240
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-04-09 17:28:27 +0530

    refactor(ExamerFirebaseMessagingService): add logic to save new token

--- app/src/main/java/com/example/examer/notifications/ExamerFirebaseMessagingService.kt
+++ app/src/main/java/com/example/examer/notifications/ExamerFirebaseMessagingService.kt
@@ -1,9 +1,17 @@
 package com.example.examer.notifications
 
+import com.example.examer.di.ExamerApplication
 import com.google.firebase.messaging.FirebaseMessagingService
 
-class ExamerFirebaseMessagingService:FirebaseMessagingService() {
-    override fun onNewToken(p0: String) {
-        // TODO
+class ExamerFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(newToken: String) {
+        // this method will be automatically called. It is not possible
+        // to ensure that a user will be logged in at the time of token
+        // generation. Therefore, just store it in shared preferences.
+        // Associate the new token with the user after the user has successfully
+        // logged in.
+        (application as ExamerApplication?)?.appContainer
+            ?.preferencesManager
+            ?.saveNotificationToken(newToken)
     }
 }
