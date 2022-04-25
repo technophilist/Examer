3fbde9e00f09e352a8a471d82f87d019120f5225
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-04-25 15:24:49 +0530

    refactor(SaveNotificationTokenWorker): remove token from prefs on successful save

--- app/src/main/java/com/example/examer/data/workers/SaveNotificationTokenWorker.kt
+++ app/src/main/java/com/example/examer/data/workers/SaveNotificationTokenWorker.kt
@@ -3,8 +3,11 @@ package com.example.examer.data.workers
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.examer.data.preferences.ExamerPreferencesManager
+import com.example.examer.data.preferences.PreferencesManager
 import com.example.examer.di.ExamerApplication
 import kotlinx.coroutines.CancellationException
+import timber.log.Timber
 
 class SaveNotificationTokenWorker(
     appContext: Context,
@@ -14,9 +17,13 @@ class SaveNotificationTokenWorker(
     private val repository = appContainer.repository
     private val authenticationService = appContainer.authenticationService
     override suspend fun doWork(): Result = authenticationService.currentUser.value?.let { user ->
+        Timber.d("Working!!")
         try {
             val notificationToken = inputData.getString(KEY_NOTIFICATION_TOKEN)!!
             repository.saveNotificationTokenForUser(user, notificationToken)
+            // delete the entry from shared prefs once the notification token
+            // is saved.
+            appContainer.preferencesManager.deleteValueIfExists(ExamerPreferencesManager.NOTIFICATION_TOKEN_KEY)
             Result.success()
         } catch (exception: Exception) {
             if (exception is CancellationException) throw exception
