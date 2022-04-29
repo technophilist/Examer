d4ddb729ab9dba9f5216814246f4dff661450306
Marsh Royden <marsh@Marshs-MacBook-Air.local>
2022-04-29 15:52:35 +0530

    refactor(PreviousTestsViewModel)!: rename previousTestsList -> testResultsMap

--- app/src/main/java/com/example/examer/ui/screens/LoggedInScreen.kt
+++ app/src/main/java/com/example/examer/ui/screens/LoggedInScreen.kt
@@ -358,7 +358,7 @@ private fun NavGraphBuilder.testHistoryScreenComposable(
         TestHistoryScreen(
             swipeRefreshState = swipeRefreshState,
             onRefresh = previousTestsViewModel::refreshPreviousTestsList,
-            testResultsMap = previousTestsViewModel.previousTestsList.value
+            testResultsMap = previousTestsViewModel.testResultsMap.value
         )
     }
 }
