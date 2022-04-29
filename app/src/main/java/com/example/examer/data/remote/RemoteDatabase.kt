2772d916f563c0ed28529dae2a6cf704144eafe1
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-04-29 12:39:37 +0530

    refactor(RemoteDatabase)!: add/imp fetchResultsForTest()

--- app/src/main/java/com/example/examer/data/remote/RemoteDatabase.kt
+++ app/src/main/java/com/example/examer/data/remote/RemoteDatabase.kt
@@ -2,15 +2,13 @@ package com.example.examer.data.remote
 
 import android.graphics.Bitmap
 import android.net.Uri
-import com.example.examer.data.domain.ExamerUser
-import com.example.examer.data.domain.TestDetails
-import com.example.examer.data.domain.UserAnswers
-import com.example.examer.data.domain.WorkBook
+import com.example.examer.data.domain.*
 import com.example.examer.data.dto.WorkBookDTO
 
 interface RemoteDatabase {
     suspend fun fetchScheduledTestListForUser(user: ExamerUser): List<TestDetails>
     suspend fun fetchPreviousTestListForUser(user: ExamerUser): List<TestDetails>
+    suspend fun fetchResultsForTest(user: ExamerUser, testDetailsId: String): TestResult
     suspend fun fetchWorkBookList(
         user: ExamerUser,
         testDetails: TestDetails
@@ -22,6 +20,7 @@ interface RemoteDatabase {
         userAnswers: UserAnswers,
         testDetailsId: String
     )
-    suspend fun markTestAsCompleted(user: ExamerUser,testDetailsId: String)
-    suspend fun markTestAsMissed(user: ExamerUser,testDetailsId: String)
+
+    suspend fun markTestAsCompleted(user: ExamerUser, testDetailsId: String)
+    suspend fun markTestAsMissed(user: ExamerUser, testDetailsId: String)
 }
