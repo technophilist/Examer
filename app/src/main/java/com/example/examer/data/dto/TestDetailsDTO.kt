6d09dcff8e1be3a9897c6bd36a0cb91b32925933
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-18 23:24:43 +0530

    refactor(TestDetailsDTO): add toTestDetails mapper extension function

--- app/src/main/java/com/example/examer/data/dto/TestDetailsDTO.kt
+++ app/src/main/java/com/example/examer/data/dto/TestDetailsDTO.kt
@@ -1,6 +1,10 @@
 package com.example.examer.data.dto
 
+import com.example.examer.data.domain.Status
 import com.example.examer.data.domain.TestDetails
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 /**
  * A DTO object equivalent to [TestDetails].
@@ -16,3 +20,20 @@ data class TestDetailsDTO(
     val testStatus: String,
     val maximumMarks: String
 )
+
+fun TestDetailsDTO.toTestDetails() = TestDetails(
+    id = id,
+    title = title,
+    description = description,
+    language = language,
+    localDateTime = getLocalDateTimeForTimeStamp(timeStamp.toLong()),
+    totalNumberOfWorkBooks = totalNumberOfWorkBooks.toInt(),
+    testDurationInMinutes = testDurationInMinutes.toInt(),
+    testStatus = Status.valueOf(testStatus.uppercase())
+)
+
+private fun getLocalDateTimeForTimeStamp(timestamp: Long): LocalDateTime =
+    Instant
+        .ofEpochMilli(timestamp)
+        .atZone(ZoneId.systemDefault())
+        .toLocalDateTime()
