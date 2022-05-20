00e753d9a7add3e56ead5e0afdb436db3d5c6af8
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-20 21:49:06 +0530

    fix(AuthenticationService): make nested classes of UpdateAttribute extend the sealed class

--- app/src/main/java/com/example/examer/auth/AuthenticationService.kt
+++ app/src/main/java/com/example/examer/auth/AuthenticationService.kt
@@ -12,10 +12,10 @@ import com.example.examer.data.domain.ExamerUser
 interface AuthenticationService {
     enum class UpdateAttributeType { NAME, EMAIL, PASSWORD, PROFILE_PHOTO_URI }
     sealed class UpdateAttribute {
-        data class Name(val newName: String, val password: String)
-        data class Email(val newEmail: String, val password: String)
-        data class Password(val newPassword: String, val oldPassword: String)
-        data class ProfilePhotoUri(val newPhotoUri: Uri)
+        data class Name(val newName: String, val password: String) : UpdateAttribute()
+        data class Email(val newEmail: String, val password: String) : UpdateAttribute()
+        data class Password(val newPassword: String, val oldPassword: String) : UpdateAttribute()
+        data class ProfilePhotoUri(val newPhotoUri: Uri) : UpdateAttribute()
     }
 
     /**
