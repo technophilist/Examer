3ebad11dbcfba70603848c677edca01b1cf7431b
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 16:19:11 +0530

    refactor: deprecate AudioPlaybackState and add PlaybackState class

--- app/src/main/java/com/example/examer/ui/screens/listenToAudioScreen/ListenToAudioScreenStates.kt
+++ app/src/main/java/com/example/examer/ui/screens/listenToAudioScreen/ListenToAudioScreenStates.kt
@@ -1,13 +1,21 @@
 package com.example.examer.ui.screens.listenToAudioScreen
 
-import androidx.annotation.FloatRange
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.*
 
+@Deprecated("Use PlaybackState class.", replaceWith = ReplaceWith("PlaybackState()"))
 data class AudioPlaybackState(
     val isEnabled: State<Boolean>,
     /*@FloatRange(from = 0.0, to = 1.0)*/ val progress: State<Float>,
     val numberOfRepeatsLeft: State<Int>
 )
 
+class PlaybackState(
+    isPlaybackEnabled: Boolean = true,
+    /*@FloatRange(from = 0.0, to = 1.0)*/
+    startProgress: Float = 0.0f
+) {
+    var isEnabled by mutableStateOf(isPlaybackEnabled)
+
+    /*@FloatRange(from = 0.0, to = 1.0)*/
+    var currentProgress by mutableStateOf(startProgress)
+}
