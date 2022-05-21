aef58317e37965233965b8203798a4ab46fdd0da
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 10:19:24 +0530

    docs: add missing docs for contents in UserAnswers.kt

--- app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
+++ app/src/main/java/com/example/examer/data/domain/UserAnswers.kt
@@ -3,19 +3,36 @@ package com.example.examer.data.domain
 import com.example.examer.data.dto.UserAnswersDTO
 import kotlinx.serialization.Serializable
 
+/**
+ * A value class that models the index of the option that is
+ * chosen by the user for a [MultiChoiceQuestion].
+ */
 @Serializable
 @JvmInline
 value class IndexOfChosenOption(val value: Int) {
     override fun toString(): String = value.toString()
 }
 
+/**
+ * A class that models the answers that are chosen by the user for a
+ * particular [WorkBook].
+ * @param associatedWorkBookId the id of the associated [WorkBook].
+ * @param answers a map that contains  the [IndexOfChosenOption] for
+ * each [MultiChoiceQuestion].
+ * @param marksObtainedForWorkBook indicates the total marks obtained
+ * for the [WorkBook] with the [associatedWorkBookId].
+ */
 @Serializable
 data class UserAnswers(
     val associatedWorkBookId: String,
     val answers: Map<MultiChoiceQuestion, IndexOfChosenOption>,
-    val marksObtainedForWorkBook:Int
+    val marksObtainedForWorkBook: Int
 )
 
+/**
+ * A converter method that is used to convert an instance of
+ * [UserAnswers] to an instance of [UserAnswersDTO].
+ */
 fun UserAnswers.toUserAnswersDTO() = UserAnswersDTO(
     associatedWorkBookId = associatedWorkBookId,
     answersDetailsMap = answers.keys.map {
