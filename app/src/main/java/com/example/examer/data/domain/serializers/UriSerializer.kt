97185337e5b549728d1f706fea25e9cbe51aeabb
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 09:58:07 +0530

    docs(UriSerializer): add doc for class

--- app/src/main/java/com/example/examer/data/domain/serializers/UriSerializer.kt
+++ app/src/main/java/com/example/examer/data/domain/serializers/UriSerializer.kt
@@ -5,8 +5,13 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
 
-
+/**
+ * A serializer for an instance of [Uri].
+ * The instance will be serialized by encoding it as a [String].
+ * It will be deserialized by parsing the string to a [Uri] object.
+ */
 @Serializer(forClass = Uri::class)
 class UriSerializer : KSerializer<Uri> {
     override fun serialize(encoder: Encoder, value: Uri) {
