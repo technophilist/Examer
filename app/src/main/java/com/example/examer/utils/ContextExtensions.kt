2ec71de272db49bd9e73cb1d9809b76d539abbbc
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 11:10:37 +0530

    docs(ContextExtensions): add docs

--- app/src/main/java/com/example/examer/utils/ContextExtensions.kt
+++ app/src/main/java/com/example/examer/utils/ContextExtensions.kt
@@ -2,12 +2,22 @@ package com.example.examer.utils
 
 import android.content.Context
 
+/**
+ * An extension function that returns true if the device's date/time
+ * are set automatically by Android. It returns false, if the device's
+ * date/time is set manually by the user.
+ */
 fun Context.isDeviceAutomaticDateTimeEnabled() = android.provider.Settings.Global.getInt(
     contentResolver,
     android.provider.Settings.Global.AUTO_TIME,
     0
 ) == 1
 
+/**
+ * An extension function that returns true if the device's time zone
+ * is set automatically by Android. It returns false, if the the device's
+ * time zone is set manually by the user.
+ */
 fun Context.isDeviceAutomaticTimeZoneEnabled() = android.provider.Settings.Global.getInt(
     contentResolver,
     android.provider.Settings.Global.AUTO_TIME_ZONE,
