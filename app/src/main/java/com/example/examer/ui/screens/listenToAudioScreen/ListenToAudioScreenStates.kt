d8760b79d251e60860bca088193a667062247b3a
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 16:36:12 +0530

    refactor: remove AudioPlaybackState class

--- app/src/main/java/com/example/examer/ui/screens/listenToAudioScreen/ListenToAudioScreenStates.kt
+++ app/src/main/java/com/example/examer/ui/screens/listenToAudioScreen/ListenToAudioScreenStates.kt
@@ -2,13 +2,6 @@ package com.example.examer.ui.screens.listenToAudioScreen
 
 import androidx.compose.runtime.*
 
-@Deprecated("Use PlaybackState class.", replaceWith = ReplaceWith("PlaybackState()"))
-data class AudioPlaybackState(
-    val isEnabled: State<Boolean>,
-    /*@FloatRange(from = 0.0, to = 1.0)*/ val progress: State<Float>,
-    val numberOfRepeatsLeft: State<Int>
-)
-
 class PlaybackState(
     isPlaybackEnabled: Boolean = true,
     /*@FloatRange(from = 0.0, to = 1.0)*/
