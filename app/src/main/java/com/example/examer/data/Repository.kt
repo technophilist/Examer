dc9503742e4c5d560d934c2ddbd127b03ba6c202
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 10:54:21 +0530

    docs: add missing docs for Repository and its concrete impl

--- app/src/main/java/com/example/examer/data/Repository.kt
+++ app/src/main/java/com/example/examer/data/Repository.kt
@@ -16,26 +16,77 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.net.URL
 
+/**
+ * An interface that contains the requisite methods for an instance
+ * of [Repository].
+ */
 interface Repository {
+    /**
+     * Used to the save the profile picture (represented as a [Bitmap])
+     * for the specified [user].
+     */
     suspend fun saveProfilePictureForUser(user: ExamerUser, bitmap: Bitmap)
+
+    /**
+     * Used to fetch a list of active tests for the specified [user].
+     * An active test is any instance of [TestDetails] with [TestDetails.testStatus]
+     * set to [Status.OPEN] or [Status.SCHEDULED]. The returned list will contain
+     * both, tests which are open and tests which are scheduled.
+     */
     suspend fun fetchScheduledTestListForUser(user: ExamerUser): List<TestDetails>
+
+    /**
+     * Used to fetch a list of previous tests for the specified [user].
+     *
+     * A previous test is any instance of [TestDetails] with
+     * [TestDetails.testStatus] set to either [Status.MISSED] or
+     * [Status.COMPLETED]. The returned list will contain both, tests
+     * which are missed and tests which are completed.
+     */
     suspend fun fetchPreviousTestListForUser(user: ExamerUser): List<TestDetails>
+
+    /**
+     * Used to fetch the [TestResult] of a single test with the
+     * specified [testDetailsId] for a specific [user].
+     */
     suspend fun fetchTestResults(user: ExamerUser, testDetailsId: String): TestResult
+
+    /**
+     * Used to fetch a list of [WorkBook]s associated with a specific
+     * [TestDetails] object, for the specified [user]. An instance of
+     * [Result] containing the list is returned.
+     */
     suspend fun fetchWorkBookList(
         user: ExamerUser,
         testDetails: TestDetails
     ): Result<List<WorkBook>>
 
+    /**
+     * Used to save the [userAnswers] associated with a particular test,
+     * with the specified [testDetailId], for the specified [user].
+     */
     suspend fun saveUserAnswersForUser(
         user: ExamerUser,
         userAnswers: UserAnswers,
         testDetailId: String
     )
 
+    /**
+     * Used to mark a test with the associated [testDetailId] as complete.
+     * @param user indicates the user who is taking the test.
+     */
     suspend fun markTestAsCompleted(user: ExamerUser, testDetailId: String)
+
+    /**
+     * Used to mark a test with the associated [testDetailId] as missed.
+     * @param user indicates the user who is taking the test.
+     */
     suspend fun markTestAsMissed(user: ExamerUser, testDetailId: String)
 }
 
+/**
+ * A concrete implementation of [Repository].
+ */
 class ExamerRepository(
     private val context: Context,
     private val remoteDatabase: RemoteDatabase,
@@ -101,17 +152,29 @@ class ExamerRepository(
         testDetailsId: String
     ): TestResult = remoteDatabase.fetchResultsForTest(user, testDetailsId)
 
+    /**
+     * Used to convert an instance of [AudioFileDTO] to an instance
+     * of [ExamerAudioFile].
+     */
     private fun AudioFileDTO.toExamerAudioFile(localAudioFileUri: Uri) = ExamerAudioFile(
         localAudioFileUri = localAudioFileUri,
         numberOfRepeatsAllowedForAudioFile = numberOfRepeatsAllowedForAudioFile
     )
 
+    /**
+     * Used to convert an instance of [WorkBookDTO] to an instance
+     * of [WorkBook].
+     */
     private fun WorkBookDTO.toWorkBook(audioFile: ExamerAudioFile) = WorkBook(
         id = id,
         audioFile = audioFile,
         questions = questions.map { it.toMultiChoiceQuestion() }
     )
 
+    /**
+     * Used to save the an audio file stored in the specified [url]
+     * to the specified [fileName].
+     */
     @Suppress("BlockingMethodInNonBlockingContext")
     private suspend fun saveAudioFileToInternalStorage(
         url: URL,
