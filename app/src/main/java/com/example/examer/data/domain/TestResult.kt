7b99333422f5f53efe1d6f6af40f91e01212ad0f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 10:12:45 +0530

    docs(TestResult): add doc for class

--- app/src/main/java/com/example/examer/data/domain/TestResult.kt
+++ app/src/main/java/com/example/examer/data/domain/TestResult.kt
@@ -1,5 +1,15 @@
 package com.example.examer.data.domain
 
+/**
+ * A class that models the results of single test.
+ *
+ * @param testDetailsId the id of the [TestDetails] object that this
+ * instance is associated with. In other words, it indicates
+ * the test that the marks are associated to.
+ * @param marksObtained indicates the total marks obtained by the user
+ * for the test.
+ * @param maximumMarks indicates the maximum marks that can be obtained.
+ */
 data class TestResult(
     val testDetailsId: String,
     val marksObtained: Int,
