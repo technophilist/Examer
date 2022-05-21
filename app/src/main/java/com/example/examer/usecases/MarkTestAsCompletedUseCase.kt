1b6e0d41008cbe416905a9da04b084387953d188
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 11:04:50 +0530

    docs(MarkTestAsCompletedUseCase): add doc for interface & concrete impl

--- app/src/main/java/com/example/examer/usecases/MarkTestAsCompletedUseCase.kt
+++ app/src/main/java/com/example/examer/usecases/MarkTestAsCompletedUseCase.kt
@@ -3,14 +3,25 @@ package com.example.examer.usecases
 import com.example.examer.auth.AuthenticationService
 import com.example.examer.data.Repository
 
+/**
+ * An interface that specifies the requisite methods for a
+ * concrete implementation of [MarkTestAsCompletedUseCase].
+ */
 interface MarkTestAsCompletedUseCase {
     suspend fun invoke(testDetailsId: String)
 }
 
+/**
+ * A concrete implementation of [MarkTestAsCompletedUseCase].
+ * @param authenticationService an instance of [AuthenticationService]
+ * that will be used to get the [AuthenticationService.currentUser].
+ * @param repository an instance of [Repository] that will be used to
+ * mark the test as complete.
+ */
 class ExamerMarkTestAsCompletedUseCase(
     private val authenticationService: AuthenticationService,
     private val repository: Repository
-):MarkTestAsCompletedUseCase {
+) : MarkTestAsCompletedUseCase {
     override suspend fun invoke(testDetailsId: String) {
         authenticationService.currentUser.value?.let { user ->
             repository.markTestAsCompleted(user, testDetailsId)
