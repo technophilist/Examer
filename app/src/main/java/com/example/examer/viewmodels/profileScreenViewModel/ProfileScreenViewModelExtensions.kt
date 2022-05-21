2de0adae351e5ecf8db6c1e2556b058f54869f0c
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-05-21 11:24:23 +0530

    docs: add docs for methods in ProfileScreenViewModelExtensions.kt

--- app/src/main/java/com/example/examer/viewmodels/profileScreenViewModel/ProfileScreenViewModelExtensions.kt
+++ app/src/main/java/com/example/examer/viewmodels/profileScreenViewModel/ProfileScreenViewModelExtensions.kt
@@ -1,6 +1,12 @@
 package com.example.examer.viewmodels.profileScreenViewModel
 
-
+/**
+ * A utility method that is used to update the name of the currently
+ * logged-in user with the [newName]. This extension method is a
+ * shorthand for the [ProfileScreenViewModel.updateAttributeForCurrentUser].
+ * It removes the need for specifying the 'updateAttribute' param of the
+ * [ProfileScreenViewModel.updateAttributeForCurrentUser] method.
+ */
 fun ProfileScreenViewModel.updateName(
     newName: String,
     resetStateTimeOut: Long = defaultResetStateTimeOut
@@ -12,6 +18,13 @@ fun ProfileScreenViewModel.updateName(
     )
 }
 
+/**
+ * A utility method that is used to update the email of the currently
+ * logged-in user with the [newEmail]. This extension method is a
+ * shorthand for the [ProfileScreenViewModel.updateAttributeForCurrentUser].
+ * It removes the need for specifying the 'updateAttribute' param of the
+ * [ProfileScreenViewModel.updateAttributeForCurrentUser] method.
+ */
 fun ProfileScreenViewModel.updateEmail(
     newEmail: String,
     resetStateTimeOut: Long = defaultResetStateTimeOut
@@ -23,6 +36,13 @@ fun ProfileScreenViewModel.updateEmail(
     )
 }
 
+/**
+ * A utility method that is used to update the password of the currently
+ * logged-in user with the [newPassword]. This extension method is a
+ * shorthand for the [ProfileScreenViewModel.updateAttributeForCurrentUser].
+ * It removes the need for specifying the 'updateAttribute' param of the
+ * [ProfileScreenViewModel.updateAttributeForCurrentUser] method.
+ */
 fun ProfileScreenViewModel.updatePassword(
     newPassword: String,
     resetStateTimeOut: Long = defaultResetStateTimeOut
